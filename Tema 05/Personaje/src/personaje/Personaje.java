bced249c70cda2569a92a7be679732305c29dee1
Rosangela <114507344+vjp-rosangelaRC@users.noreply.github.com>
2023-01-11 16:09:26 +0100

    Ejercicio personaje

--- Tema 05/Personaje/src/personaje/Personaje.java
+++ Tema 05/Personaje/src/personaje/Personaje.java
@@ -10,12 +10,54 @@ package personaje;
  * @author Ross
  */
 public class Personaje {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private String nombre;
+    private int fuerza;
+    private int nivel;
+    
+    public String getNombre(){
+        return nombre;
     }
     
+    public void setNombre(String _nombre){
+        nombre = _nombre;
+    }
+    
+    public int getFuerza(){
+        return fuerza;
+    }
+    
+    public void setFuerza(int _fuerza){
+        fuerza = _fuerza;
+    }
+    
+    public int getNivel(){
+        return nivel;
+    }
+    
+    public void setNivel(int _nivel){
+        nivel = _nivel;
+    }
+    
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Personaje personaje = new Personaje();
+        String nombre = "pepe";
+        int fuerza = 5, nivel = 3;
+        
+        personaje.setNombre(nombre);
+        personaje.setFuerza(fuerza);
+        personaje.setNivel(nivel);
+        
+        System.out.println("El nombre del personaje es " + personaje.getNombre() + ", su fuerza es " + personaje.getFuerza() + " y su nivel es " + personaje.getNivel() + ".");
+        
+    }
+    
+    
+    /*
+      Para probar el programa, en el main os creareis un objeto de tipo personaje y le daremos valores a sus atributos a partir de los setters y mostrareis dichos valores haciendo uso de los getters.
+
+  Un saludo.
+    */
 }
