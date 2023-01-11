b3ff1564497c8652d51ff754e3ab07fab87494fa
Rosangela <114507344+vjp-rosangelaRC@users.noreply.github.com>
2023-01-11 20:53:22 +0100

    .

---
+++ Tema 05/T05E15/src/t05e15/Direccion.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package t05e15;
+
+/**
+ *
+ * @author rdlrosac01
+ */
+public class Direccion {
+
+    private String calle;
+    private int numero;
+    private int piso;
+    private String ciudad;
+    
+    
+    
+    
+    
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
