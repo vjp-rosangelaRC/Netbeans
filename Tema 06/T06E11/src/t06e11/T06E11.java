34936baf2ceba42f94dea5dde3e67453b2cb7e13
Rosangela <114507344+vjp-rosangelaRC@users.noreply.github.com>
2023-01-27 13:39:19 +0100

    .

--- Tema 06/T06E11/src/t06e11/T06E11.java
+++ Tema 06/T06E11/src/t06e11/T06E11.java
@@ -14,8 +14,32 @@ public class T06E11 {
     /**
      * @param args the command line arguments
      */
+    public static void rellenarVector(int vector[]) {
+        int i;
+
+        for (i = 0; i < vector.length; i++) {
+            vector[i] = (int) (Math.random() * 9);
+            // Falta lo de la repetición, no funciona nada
+        }
+    }
+    
+    
+    public static void mostrarVector (int [] vector){
+        for (int i = 0; i < vector.length; i++) {
+            System.out.println(vector[i]);
+        }
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        int[] vector = new int[10];
+        
+        System.out.println("Los números generados son:");
+        rellenarVector(vector);
+        
+        mostrarVector(vector);
+        
+        
     }
     
 }
