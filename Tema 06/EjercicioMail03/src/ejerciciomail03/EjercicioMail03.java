591b1b4a3238579f4ebbf55db9e97b91b5ba06db
Rosangela <114507344+vjp-rosangelaRC@users.noreply.github.com>
2023-01-30 13:38:40 +0100

    Mail 03

--- Tema 06/EjercicioMail03/src/ejerciciomail03/EjercicioMail03.java
+++ Tema 06/EjercicioMail03/src/ejerciciomail03/EjercicioMail03.java
@@ -5,6 +5,8 @@
  */
 package ejerciciomail03;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Ross
@@ -14,8 +16,53 @@ public class EjercicioMail03 {
     /**
      * @param args the command line arguments
      */
+    public static int pedirN(){
+        Scanner entrada = new Scanner(System.in);
+        int n = entrada.nextInt();
+        return n;
+    }
+    
+    public static int generarAleatorio(){
+        int aleatorio = (int) ((Math.random()*300)+1) ;
+        return aleatorio;
+    }
+    
+    public static int[] rellenarVector(int [] vector){
+        int i;
+        
+        for (i = 0; i < vector.length; i++){
+            vector [i] = generarAleatorio();
+            // System.out.println("El valor generado es: " + vector[i]);
+        } 
+        
+        return vector;
+    }
+    
+    public static void mostrarVector(int[] vector){
+        System.out.println("¿Digito final?");
+        int n = pedirN();
+        
+        for (int i = 0; i < vector.length; i++){
+            //String numero = vector[i] + "";
+            int resto = vector[i] % 10;
+            if (resto == n){
+                System.out.println("Coinciden en el último dígito: " + vector[i]);
+            } /*else {
+                System.out.println("aaa");
+            }*/
+        }       
+        
+    }
+    
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("¿Tamaño del vector?");
+        int n = pedirN();
+        int [] vector = new int[n];
+                
+        rellenarVector(vector);
+        mostrarVector(vector);
     }
     
 }
