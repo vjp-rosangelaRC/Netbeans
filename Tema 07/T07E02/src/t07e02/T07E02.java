1712de52e5917fb057c09f8525faf19509def01c
Rosangela <114507344+vjp-rosangelaRC@users.noreply.github.com>
2023-02-06 14:01:20 +0100

    T07E02

--- Tema 07/T07E02/src/t07e02/T07E02.java
+++ Tema 07/T07E02/src/t07e02/T07E02.java
@@ -5,6 +5,8 @@
  */
 package t07e02;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Ross
@@ -14,8 +16,60 @@ public class T07E02 {
     /**
      * @param args the command line arguments
      */
+    public static int generarAleatorio(){
+        int aleatorio;
+        aleatorio = (int) (Math.random() * 6 + 1);
+        return aleatorio;
+    }
+    
+    public static void rellenarVector(int vector[]) {
+        int i;
+
+        for (i = 0; i < vector.length; i++) {
+            vector[i] = generarAleatorio();
+        }
+    }
+    
+    public static void mostrarVector (int [] vector){ //Con for each
+        for (int n:vector){
+            System.out.println(n);
+        }
+    }
+    
+    public static int longitudUsuario() {
+        Scanner entrada = new Scanner(System.in);
+
+        int longitud = 0;
+
+        do {
+            System.out.println("Por favor, introduzca una longitud para el vector:");
+            try {
+                longitud = entrada.nextInt();
+            } catch (Exception e) {
+                System.out.println(e);
+                System.out.println("Introduzca un número entero");
+                longitud = -1; //Le doy un valor -1 para que siga pidiendo longitud
+            } finally {
+                entrada.nextLine(); //Esto limpia el buffer
+            } 
+        } while (longitud < 1 || longitud > 10);
+        return longitud;
+    }
+    
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        int longitud, aleatorio;
+        longitud = longitudUsuario();
+        aleatorio = generarAleatorio();
+        
+        int[] vector = new int[longitud];
+        
+        rellenarVector(vector);
+        
+        System.out.println("El vector generado es");
+        mostrarVector(vector);
+        
     }
     
 }
