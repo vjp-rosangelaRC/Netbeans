76922f62f55b2f53fd3e9372be3ca24e26c22693
Rosangela <114507344+vjp-rosangelaRC@users.noreply.github.com>
2023-03-01 15:34:28 +0100

    Parte 07

--- Tema 10/T10E06/src/t10e06/T10E06.java
+++ Tema 10/T10E06/src/t10e06/T10E06.java
@@ -5,6 +5,9 @@
  */
 package t10e06;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author Ross
@@ -14,8 +17,98 @@ public class T10E06 {
     /**
      * @param args the command line arguments
      */
+    public static int MAX = 12;
+    
+    public static int pedirN() {
+        Scanner entrada = new Scanner(System.in);
+        int n; 
+        n = entrada.nextInt(); 
+        return n;     
+    }
+    
+    public static void introducirVentas(String[] mesesAnho, ArrayList lista) {
+        int n; 
+        for (int i=0; i < mesesAnho.length; i++){
+            System.out.println("Introduzca las ventas para " + mesesAnho[i]);
+            n = pedirN();
+            lista.add(n);
+        }
+    }
+    
+    public static void mostrarVentas(String[] mesesAnho, ArrayList lista) {
+        System.out.println("Las ventas ha sido:");
+        
+        for (int i=0; i < mesesAnho.length; i++){
+            System.out.println("Para " + mesesAnho[i] + ": " + lista.get(i));
+        }
+    }
+    
+    public static void mostrarVentasAlReves(String[] mesesAnho, ArrayList lista) {
+        System.out.println("Las ventas ha sido:");
+
+        for (int i = 12; i > mesesAnho.length; i--){
+            System.out.println("Para " + mesesAnho[i] + ": " + lista.get(i));
+        }
+    }
+    
+    public static void sumaTotal (ArrayList lista) {
+        int suma = 0; 
+        for (int i=0; i < MAX; i++){
+            suma = suma + (int) lista.get(i);
+        }
+        System.out.println("El total ha sido: " + suma);
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        int opcion;
+        
+        String[] mesesAnho = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+        ArrayList lista = new ArrayList();
+        
+        do {
+            System.out.println("1. Introducir las ventas de coches de cada uno de los 12 meses del año.");
+            System.out.println("2. Mostrar las ventas introducidas en el punto anterior,");
+            System.out.println("3. Mostrar las ventas introducidas al revés.");
+            System.out.println("4. Que muestre la suma total de ventas del año");
+            System.out.println("5. Que mietre las ventas totales de los meses cuyo nombre contenga la letra a.");
+            System.out.println("6. Que muestre el nombre del mes (o meses) con más ventas");
+            System.out.println("7. Salir del programa");
+            opcion = pedirN();
+            
+            switch(opcion){
+                case 1: {
+                    introducirVentas(mesesAnho, lista);
+                    break;
+                }
+                
+                case 2: {
+                    mostrarVentas(mesesAnho, lista);
+                    break;
+                }
+                
+                case 3: {
+                    mostrarVentasAlReves(mesesAnho, lista); //Este no va 
+                    break;
+                }
+                
+                case 4: {
+                    sumaTotal(lista);
+                    break;
+                }
+                
+                case 7:{
+                    System.out.println("Saliendo ...");
+                    break;
+                }
+                default: {
+                    System.out.println("No es una opción válida");
+                }
+            }
+        }while (opcion != 7);
+        
+        
     }
     
 }
