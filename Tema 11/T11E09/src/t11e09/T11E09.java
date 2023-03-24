d5052ad40bbee16631c20e4204c6b00c564fb2de
Rosangela <114507344+vjp-rosangelaRC@users.noreply.github.com>
2023-03-24 20:11:15 +0100

    Clase

--- Tema 11/T11E09/src/t11e09/T11E09.java
+++ Tema 11/T11E09/src/t11e09/T11E09.java
@@ -16,6 +16,40 @@ public class T11E09 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        /*
+        Tenemos un atributo que es date. 
+        Luego tengo un mapa de tipo TreeMap con las fechas como claves y asociamos 
+        el objeto de tipo sorteo. Los TreeMaps se ordenan por defecto de menor a 
+        mayor clave; los hastMap no tienen orden, los linkedHaskMap se orden en 
+        función del orden que he hecho. 
+        Para el mes actual tengo que hacer lo de local day y ver si el mes de la clave
+        coincide con el mes actual. 
+        Para mostrar el sorteo no me tengo que recorrer nada porque ya tengo una clave,
+        pido el objeto asociado a esa clave. Usamos el método containskay en este caso 
+        también. 
+        El de realizar sorteo pide una fecha y genera si el sorteo existe o no, como antes
+        y luego genera el sorteo. 
+        En este caso em el menú el realizar sorteo y el repetir sorteo los ha puesto 
+        iguales (con el break al final del 2, y puesto en el 2). Porque sobreescribimos
+        los datos, funcionan igual. 
+        Realizar sorteo crea un nuevo vector con el constructor y kuego hace el put para 
+        el mapa. 
+        Para saber el mes actual necesito recoger la fecha del día y recibe el mapa. Creo
+        una variable para la fecha actual y luego me recorro el mapa entero y todas las claves 
+        cuyo mes coincida con el actual lo muestro. 
+        Aquí nos recorremos todas las claves, y recojo el sorteo. 
+        Le paso la clave y con eso me retorna el elemetno asociaado a esa clave. Y con un 
+        if veo si coincide o no. 
+        En el mostrar sorteo según fecha podemos hacer una búsqueda directamente con 
+        el método contains key. 
+        Para el de realizar sorte por fecha tengo que comprobar primero si tengo esa 
+        clave ya.
+        Mostrar sorteo es recorrer un para entero, por las claves. 
+        
+        
+        
+        
+        */
     }
     
 }
