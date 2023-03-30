dceaa8914e18490c16f321e2757ace52e24e6447
Rosangela <114507344+vjp-rosangelaRC@users.noreply.github.com>
2023-03-30 16:48:32 +0200

    HECHO EN CLASE

--- Entornos/ED_UT4_Proyecto/src/ed_ut4_proyecto/Pelicula.java
+++ Entornos/ED_UT4_Proyecto/src/ed_ut4_proyecto/Pelicula.java
@@ -1,11 +1,21 @@
-
+/**
+ * Esta clase contiene los atributos y métodos de la clase película
+ *
+ * @author Rosangela de la Rosa
+ * @version 2.0
+ */
 package ed_ut4_proyecto;
 
 public class Pelicula {
+    /**
+     * Atributos de la clase Película
+     */
     private String titulo;
     private float costeLicencia;
     private Socio[] vSocios;
-
+/**
+     * Constructor por defecto
+     */
     public Pelicula() {
         this.titulo = "";
         this.costeLicencia = 0;
@@ -14,7 +24,11 @@ public class Pelicula {
             this.vSocios[i] = new Socio();
         }
     }
-    
+    /**
+     * Constructor parametrizado
+     * @param titulo → título de la película que quiero guardar
+     * @param costeLicencia → precio de la licencia de la película
+     */
     public Pelicula(String titulo, float costeLicencia) {
         this.titulo = titulo;
         this.costeLicencia = costeLicencia;
@@ -23,7 +37,9 @@ public class Pelicula {
             this.vSocios[i] = new Socio();
         }
     }
-    
+    /**
+     * Método que nos muestra las películas (título de la película y el coste de la licencia) usando un bucle for
+     */
     public void mostrarPelicula(){
         System.out.println("Titulo: "+this.titulo);
         System.out.println("Coste licencia: "+this.costeLicencia);
@@ -31,7 +47,10 @@ public class Pelicula {
             System.out.println("    Socio: "+this.vSocios[i].getNombre()+" - Precio abonado: "+this.vSocios[i].getPrecioAbonado());
         }
     }
-    
+    /**
+     * Método que nos devuelve la rentabilidad de una película
+     * @return valor que corresponde a los ingresos - coste de la licencia 
+     */
     public float getRentabilidad(){
         float rentabilidad, ingresos = 0;
         for(int i = 0; i < this.vSocios.length;i++){
@@ -40,27 +59,45 @@ public class Pelicula {
         rentabilidad = ingresos - this.costeLicencia;
         return rentabilidad;
     }
-
+/**
+ * Método para obtener el título de una película
+ * @return nos devuelve el título de la película 
+ */
     public String getTitulo() {
         return titulo;
     }
-
+/**
+ * Método para establecer el título de película
+ * @param titulo → Título de la película que queremos guardar
+ */
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
-
+/**
+ * Método para obtener el coste de la licencia
+ * @return nos devuelve el valor del coste de la licencia 
+ */
     public float getCosteLicencia() {
         return costeLicencia;
     }
-
+/**
+ * Método para establecer el valor del coste de la licencia 
+ * @param costeLicencia → valor que queremos que tenga el coste de la licencia
+ */
     public void setCosteLicencia(float costeLicencia) {
         this.costeLicencia = costeLicencia;
     }
-
+/**
+ * Método para recuperar los socios que tenemos en cada película
+ * @return un vector de socios con 2 celdas
+ */
     public Socio[] getvSocios() {
         return vSocios;
     }
-
+/**
+ * Método para añadir información al vector de socios
+ * @param vSocios → Introducimos información a los socios (nombre del socio que se quiere registrar y precio que abona cada socio para ver películas)
+ */
     public void setvSocios(Socio[] vSocios) {
         this.vSocios = vSocios;
     }
--- Entornos/ED_UT4_Proyecto/src/ed_ut4_proyecto/Socio.java
+++ Entornos/ED_UT4_Proyecto/src/ed_ut4_proyecto/Socio.java
@@ -1,34 +1,72 @@
-
+/**
+ * Esta clase contiene los atributos y métodos de la clase socio
+ *
+ * @author Rosangela de la Rosa
+ * @version 1.0
+ */
 package ed_ut4_proyecto;
 
 public class Socio {
+
+    /**
+     * Atributos de la clase Socio
+     */
     private String nombre;
     private float precioAbonado;
 
+    /**
+     * Constructor por defecto
+     */
     public Socio() {
         this.nombre = "";
         this.precioAbonado = 0;
     }
-    
+
+    /**
+     * Constructor parametrizado
+     *
+     * @param nombre → nombre del socio que se quiere registrar
+     * @param precioAbonado → precio que abona cada socio para ver películas
+     */
     public Socio(String nombre, float precioAbonado) {
         this.nombre = nombre;
         this.precioAbonado = precioAbonado;
     }
 
+    /**
+     * Método para obtener el nombre del socio
+     *
+     * @return devuelve el nombre del socio
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Método para establecer el nombre del socio
+     *
+     * @param nombre → Nombre que le asignamos al socio
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * Método para obtener la cantidad que ha abonado el socio
+     *
+     * @return devuelve la cantidad que ha abonado el socio
+     */
     public float getPrecioAbonado() {
         return precioAbonado;
     }
 
+    /**
+     * Método para establecer la cantidad que abona el socio
+     *
+     * @param precioAbonado → Asignamos la cantidad que abona el socio
+     */
     public void setPrecioAbonado(float precioAbonado) {
         this.precioAbonado = precioAbonado;
     }
-    
+
 }
